699e2525870ca2284d9735be87e1e38bd45b9df7
Dominik <dominikbutz@gmail.com>
2021-02-24 14:30:12 +0800

    Initial Commit

---
+++ TrimAnimationBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrimAnimationBug
+//
+//  Created by Dominik Butz on 24/2/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrimAnimationBug/TrimAnimationBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimAnimationBugApp.swift
+//  TrimAnimationBug
+//
+//  Created by Dominik Butz on 24/2/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrimAnimationBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
